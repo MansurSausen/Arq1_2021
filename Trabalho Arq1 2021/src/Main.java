301489bb968858d86e72ccda94b84288d40697ae
wiliamkern <wiliam.franco@hotmail.com>
2021-10-16 14:20:18 -0300

    teste

--- Trabalho Arq1 2021/src/Main.java
+++ Trabalho Arq1 2021/src/Main.java
@@ -1,15 +1,13 @@
 
-
 public class Main {
 
-	public static void main(String[] args) { 
-        // Notação Polonesa Inversa
-        //Assembly calc = new Assembly();
-        String exp = "5 2 1 - +";  
-        //calc.asm(exp);        
-    System.out.println("Gerado Assembly.asm da seguinte expressão: ");     
-    System.out.println("Expressao = " + exp);    
-    System.out.println("#HENRIQUE HERMES && CARLOS VOGT");  
-}
-
+	public static void main(String[] args) {
+		// Notação Polonesa Inversa
+		// Assembly calc = new Assembly();
+		String exp = "5 2 1 - +";
+		// calc.asm(exp);
+		System.out.println("Gerado Assembly.asm da seguinte expressão: ");
+		System.out.println("Expressao = " + exp);
+		System.out.println("---- Felipe / Manser / Wiliam ----");
+	}
 }
